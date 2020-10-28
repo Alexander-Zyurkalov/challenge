cf25a93c5746e5b5a55b9833c198f00877a02504
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2020-10-28 22:11:39 +0100

    changes type of the application

--- src/main/java/com/raisin/zyurkalov/challenge/ChallengeApplication.java
+++ src/main/java/com/raisin/zyurkalov/challenge/ChallengeApplication.java
@@ -1,13 +1,28 @@
 package com.raisin.zyurkalov.challenge;
 
-import org.springframework.boot.SpringApplication;
+import com.raisin.zyurkalov.challenge.services.Source;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChallengeApplication {
 
+    @Autowired
+    Source source;
+
     public static void main(String[] args) {
-        SpringApplication.run(ChallengeApplication.class, args);
+        new SpringApplicationBuilder().sources(ChallengeApplication.class).web(WebApplicationType.NONE).run(args);
+    }
+
+    @Bean
+    CommandLineRunner run() {
+        return (evt) -> {
+            System.out.println("OK");
+        };
     }
 
 }
---
+++ src/main/java/com/raisin/zyurkalov/challenge/ChallengeApplicationConfiguration.java
@@ -0,0 +1,17 @@
+package com.raisin.zyurkalov.challenge;
+
+import com.raisin.zyurkalov.challenge.services.Source;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChallengeApplicationConfiguration {
+
+
+    @Bean
+    public Source getSource() {
+        return null;
+    }
+
+
+}
