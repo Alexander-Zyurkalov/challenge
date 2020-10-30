210fb7035212e61e4eb07351bea8b89225c581dc
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2020-10-30 01:51:49 +0100

    remove redundant

--- src/main/java/com/raisin/zyurkalov/challenge/adapters/mappers/ChallengeRecordXmlMapper.java
+++ src/main/java/com/raisin/zyurkalov/challenge/adapters/mappers/ChallengeRecordXmlMapper.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.raisin.zyurkalov.challenge.adapters.ExceptionsHolder;
-import com.raisin.zyurkalov.challenge.adapters.mappers.xml_entities.Id;
-import com.raisin.zyurkalov.challenge.adapters.mappers.xml_entities.XmlChallengeRecord;
 import com.raisin.zyurkalov.challenge.entities.ChallengeRecord;
 import com.raisin.zyurkalov.challenge.entities.Status;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,17 +39,6 @@ public class ChallengeRecordXmlMapper implements ChallengeRecordMapper {
         return exceptionsHolder;
     }
 
-    private ChallengeRecord toChallengeRecord(XmlChallengeRecord xmlChallengeRecord) {
-        Status status = xmlChallengeRecord.getDone() == null ? null : Status.valueOf(xmlChallengeRecord.getDone());
-        return new ChallengeRecord(xmlChallengeRecord.getId().value, status);
-    }
-
-    private XmlChallengeRecord toXmlChallengeRecord(ChallengeRecord object) {
-        String status = object.getStatus() == null? "" : object.getStatus().toString();
-        return new XmlChallengeRecord(
-                new Id(object.getId()), status
-        );
-    }
 
     @Autowired
     public void setExceptionsHolder(ExceptionsHolder exceptionsHolder) {
