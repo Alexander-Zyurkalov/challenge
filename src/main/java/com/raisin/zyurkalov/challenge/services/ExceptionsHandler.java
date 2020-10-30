d839552335f421a31e0f999a0a16220ed2e4034c
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2020-10-30 06:32:30 +0100

    adds documentation for the exceptions handler

--- src/main/java/com/raisin/zyurkalov/challenge/services/ExceptionsHandler.java
+++ src/main/java/com/raisin/zyurkalov/challenge/services/ExceptionsHandler.java
@@ -5,12 +5,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
+/**
+ * This is an exception handler. The main purpose of the class is to handle all the possible exceptions
+ * in one place. For example, we can add here sending opsgeine alert, e-mail, or just writing to the logs.
+ * */
 @Component
 @Slf4j
 public class ExceptionsHandler {
     @Autowired
     ExceptionsHolder exceptionsHolder;
 
+    /**
+     * Write all the code here to handle the exceptions
+     */
     public void handle() {
         exceptionsHolder.getExceptions().subscribe(
                 (e) -> {
