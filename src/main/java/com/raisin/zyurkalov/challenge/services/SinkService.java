46460d1c530846c8e0ff2f67ff778fe34cdd1f51
Alexandr Zyurkalov <alexandr.zyurkalov@raisin.com>
2020-10-30 07:01:54 +0100

    removes some old debugging code

--- src/main/java/com/raisin/zyurkalov/challenge/services/SinkService.java
+++ src/main/java/com/raisin/zyurkalov/challenge/services/SinkService.java
@@ -1,7 +1,6 @@
 package com.raisin.zyurkalov.challenge.services;
 
 import com.raisin.zyurkalov.challenge.entities.Kind;
-import com.raisin.zyurkalov.challenge.entities.Status;
 import com.raisin.zyurkalov.challenge.services.sink_service.Response;
 import com.raisin.zyurkalov.challenge.services.sink_service.Result;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -20,9 +19,6 @@ public class SinkService {
         try {
             Response response = WebClient.create(url).post().body(Mono.just(result), Result.class).retrieve()
                     .bodyToMono(Response.class).block();
-            if (response.getStatus() == Status.OK) {
-                System.out.println("ой-ой!");
-            }
             return response;
         } catch (Exception e) {
             e.printStackTrace();
